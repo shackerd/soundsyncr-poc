e4aa12fc263fc9c3f75ffd4d63ac7c6efe9a15e4
= <r.csgnl@pm.me>
2022-11-15 00:13:54 +0100

    add CLI command

--- src/Program.cs
+++ src/Program.cs
@@ -1,30 +1,54 @@
 ﻿using Spectre.Console;
-
+using Spectre.Console.Cli;
 
 namespace Midicontrol
 {
     public class Program {
         public static void Main(string[] args)
-        {
-            AnsiConsole.Markup("[underline red]xmidicontrol[/], available devices:\n");
-            Table table = new Table();
-            table.AddColumns("Id", "Interface", "Name", "Input", "Output", "InUse");
-            table.Border(TableBorder.Rounded);
-
-            // public int ID { readonly get; set; }
-            // public string Interface { get; }
-            // public string Name { get; }
-            // public bool IsInput { get; }
-            // public bool IsOutput { get; }
-            // public bool IsOpened { get; }
-
-            foreach (var device in PortMidi.MidiDeviceManager.AllDevices)
-            {
-                table.AddRow(device.ID.ToString(), device.Interface, device.Name, device.IsInput.ToString(), device.IsOutput.ToString(), device.IsOpened.ToString());
-            }
-
-            AnsiConsole.Write(table);
+        {                        
+            var app = new CommandApp();
+            app.Configure(config => {
+                config
+                    .AddCommand<DeviceCommand>("device")
+                    .WithAlias("d");
+            });
+            app.Run(args);
         }
+    }
+
+    internal class DeviceCommand : Command<DeviceCommandSettings>
+    {
+        // xmidictrl -v
+        // xmidictrl -d -l
+        // xmidictrl --device --list
+        // xmidictrl --device --list
+        // xmidictrl -c -l
+        // xmidictrl --channel --list
+        public override int Execute(CommandContext context, DeviceCommandSettings settings)
+        {
+            if(settings.List){
+                AnsiConsole.Markup("[underline red]xmidicontrol[/], available devices:\n");
+                Table table = new Table();
+                table.AddColumns("Id", "Interface", "Name", "Input", "Output", "InUse");
+                table.Border(TableBorder.Rounded);
+
+                foreach (var device in PortMidi.MidiDeviceManager.AllDevices)
+                {
+                    table.AddRow(device.ID.ToString(), device.Interface, device.Name, device.IsInput.ToString(), device.IsOutput.ToString(), device.IsOpened.ToString());
+                }
+
+                AnsiConsole.Write(table);
+            }
+            // Omitted
+            return 0;
+        }
+
+    }
+
+    internal class DeviceCommandSettings : CommandSettings
+    {
+        [CommandOption("-l|--list")]
+        public bool List { get; set; }
     }
 }
 
