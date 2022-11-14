578dc50fe2badfc0d13064a141368d66f40c8729
= <r.csgnl@pm.me>
2022-11-14 12:13:46 +0100

    add portmidi wapper lib

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+
+namespace Midicontrol
+{
+    public class Program {
+        public static void Main(string[] args)
+        {
+            AnsiConsole.Markup("[underline red]xmidicontrol[/], available devices:\n");
+            Table table = new Table();
+            table.AddColumns("Id", "Interface", "Name", "Input", "Output", "InUse");
+            table.Border(TableBorder.Rounded);
+
+            // public int ID { readonly get; set; }
+            // public string Interface { get; }
+            // public string Name { get; }
+            // public bool IsInput { get; }
+            // public bool IsOutput { get; }
+            // public bool IsOpened { get; }
+
+            foreach (var device in PortMidi.MidiDeviceManager.AllDevices)
+            {
+                table.AddRow(device.ID.ToString(), device.Interface, device.Name, device.IsInput.ToString(), device.IsOutput.ToString(), device.IsOpened.ToString());
+            }
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
+
