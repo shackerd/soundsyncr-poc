2eaa36714bb48f2e79efd160caf781ffe10ae456
= <r.csgnl@pm.me>
2022-11-23 10:51:05 +0100

    rearrange PulseAudioStream class

--- src/PulseAudio/PulseAudioStream.cs
+++ src/PulseAudio/PulseAudioStream.cs
@@ -1,32 +1,43 @@
+using System.Collections.ObjectModel;
 using Midicontrol.PulseAudio.DBus;
 
 namespace Midicontrol.PulseAudio
 {
     public class PulseAudioStream
-    {
-        public IStreamProxy Proxy { get; }        
-        public string Binary { get; }
-        public string Pid { get; }
-        public PulseAudioStreamType Type { get; }
-        public StreamProperties Properties { get; }
+    {        
+        private StreamProperties _properties;
         private const string _applicationProcessBinary = "application.process.binary";
         private const string _applicationProcessId = "application.process.id";
+        private const string _applicationProcessName = "application.process.Name";
+        internal readonly IReadOnlyDictionary<string, string> _readProperties;    
+        
+        public IStreamProxy Proxy { get; }        
+        public string Binary => _readProperties[_applicationProcessBinary];
+        public string Pid => _readProperties[_applicationProcessId];
+        public string Name => _readProperties[_applicationProcessName];
+        public PulseAudioStreamType Type { get; }
+        public IReadOnlyDictionary<string, string> Properties => _readProperties;    
 
-        internal PulseAudioStream(IStreamProxy proxy, StreamProperties properties, string appBinary, string pid, PulseAudioStreamType type)
+        internal PulseAudioStream(IStreamProxy proxy, StreamProperties properties, PulseAudioStreamType type, Dictionary<string, string> readProperties)
         {
-            Proxy = proxy;
-            Binary = appBinary;
-            Pid = pid;
+            Proxy = proxy;            
             Type = type;
-            Properties = properties;
-        }
+            _properties = properties;
+            _readProperties = new ReadOnlyDictionary<string, string>(readProperties);            
+        }        
 
         public static async Task<PulseAudioStream> CreateAsync(IStreamProxy proxy, PulseAudioStreamType type)
         {            
             StreamProperties props = await proxy.GetAllAsync();
-            var app = System.Text.Encoding.Default.GetString(GetPropertyValue(props, _applicationProcessBinary));
-            var pid = System.Text.Encoding.Default.GetString(GetPropertyValue(props, _applicationProcessId));            
-            return new PulseAudioStream(proxy, props, app, pid, type);
+
+            Dictionary<string, string> kvs = new Dictionary<string, string>();
+
+            foreach (string key in props.PropertyList.Keys)
+            {
+                kvs.Add(key, System.Text.Encoding.UTF8.GetString(GetPropertyValue(props, key)));
+            }
+
+            return new PulseAudioStream(proxy, props, type, kvs);
         }
 
         private static byte[] GetPropertyValue(StreamProperties properties, string propName) 
